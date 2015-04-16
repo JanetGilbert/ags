df663f84e6c6e2b5988767fee55d83484c839d53
Janet Gilbert <janetcampbell68@hotmail.com>
2015-04-16 17:17:16 -0400

    Added OGG project

---
+++ iOS/libraries/ogg/ogg/ogg.h
@@ -0,0 +1,13 @@
+//
+//  ogg.h
+//  ogg
+//
+//  Created by Janet Gilbert on 4/16/15.
+//  Copyright (c) 2015 WadjetEyeGames. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ogg : NSObject
+
+@end
