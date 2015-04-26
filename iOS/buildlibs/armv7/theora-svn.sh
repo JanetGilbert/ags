e53a123194852e4f12fa62c421d781c18cfbf7bf
Janet Gilbert <janetcampbell68@hotmail.com>
2015-04-26 14:28:01 -0400

    Updated and patched Theora

--- iOS/buildlibs/armv7/theora-svn.sh
+++ iOS/buildlibs/armv7/theora-svn.sh
@@ -6,25 +6,25 @@ FILENAME=libtheora-svn
 
 svn co http://svn.xiph.org/trunk/theora $FILENAME
 
-#cd $FILENAME
+cd $FILENAME
 
 # remove call to ./configure from the script
-#head --lines=-1 autogen.sh > autogenmod.sh
+head --lines=-1 autogen.sh > autogenmod.sh
 
-#chmod +x ./autogenmod.sh
+chmod +x ./autogenmod.sh
 
-#./autogenmod.sh
+./autogenmod.sh
 
-#rm ./config.sub
-#rm ./config.guess
+rm ./config.sub
+rm ./config.guess
 
-#wget http://git.savannah.gnu.org/cgit/config.git/plain/config.sub -O config.sub
-#wget http://git.savannah.gnu.org/cgit/config.git/plain/config.guess -O config.guess
+wget http://git.savannah.gnu.org/cgit/config.git/plain/config.sub -O config.sub
+wget http://git.savannah.gnu.org/cgit/config.git/plain/config.guess -O config.guess
 
-#wget https://raw.github.com/gabriel/ffmpeg-iphone-build/master/gas-preprocessor.pl -O lib/arm/gas-preprocessor.pl --no-check-certificate
-#chmod +x lib/arm/gas-preprocessor.pl
+wget https://raw.github.com/gabriel/ffmpeg-iphone-build/master/gas-preprocessor.pl -O lib/arm/gas-preprocessor.pl --no-check-certificate
+chmod +x lib/arm/gas-preprocessor.pl
 
-#patch -p0 < ../../../patches/armv7/libtheora-svn.patch
+patch -p0 < ../../../patches/armv7/libtheora-svn.patch
 
 #export CCAS="perl ./arm/gas-preprocessor.pl $DEVROOT/usr/bin/arm-apple-darwin10-gcc-4.2.1"
 
